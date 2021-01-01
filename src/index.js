d8bb5ab6e199e391290f7b458388bfd66f5e9623
Steven Frieson <sfrieson@gmail.com>
2021-01-01 16:49:53 -0500

    💫 Implements basic HTML case

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
+function createsStackingContext(el) {
+  if (el.nodeName === "HTML") return true;
+}
 function findStackingContexts(targetEl) {
   const contexts = [];
 
+  let currentElement = targetEl.parentElement;
+
+  while (currentElement) {
+    if (createsStackingContext(currentElement)) {
+      contexts.push(currentElement);
+    }
+    currentElement = currentElement.parentElement;
+  }
   return contexts;
 }
 
--- src/index.test.js
+++ src/index.test.js
@@ -7,14 +7,12 @@ describe("findStackingContexts", () => {
   // https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Positioning/Understanding_z_index/The_stacking_context#The_stacking_context
   it("returns the html node last", () => {
     const dom = new JSDOM('<html><body><div id="test-el"></div></body></html>');
-
     const testEl = dom.window.document.getElementById("test-el");
+
     const contexts = findStackingContexts(testEl);
 
     expect(contexts).toBeInstanceOf(Array);
     expect(contexts).toHaveLength(1);
-    expect(contexts).toInclude(
-      dom.window.document.getElementsByTagName("html")[0]
-    );
+    expect(contexts[0].nodeName).toBe("HTML");
   });
 });
