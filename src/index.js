2c35e4693c19619e871c4cb5ef15c5dedf53f6c2
Steven Frieson <sfrieson@gmail.com>
2021-01-02 09:28:56 -0500

    💫 Finishes basic test cases

--- src/index.js
+++ src/index.js
@@ -3,10 +3,9 @@ function createsStackingContext(el, window) {
   if (nodeName === "HTML") return true;
   const style = window.getComputedStyle(el);
   if (style.position === "absolute" || style.position === "relative") {
-    if (!style.zIndex || style.zIndex === "auto") {
-      return false;
+    if (style.zIndex && style.zIndex !== "auto") {
+      return true;
     }
-    return true;
   }
   if (style.position === "fixed" || style.position === "sticky") {
     return true;
@@ -19,6 +18,43 @@ function createsStackingContext(el, window) {
       return false;
     }
 
+    return true;
+  }
+
+  if (style.opacity !== "1" && style.opacity !== "") {
+    return true;
+  }
+
+  if (style.mixBlendMode && style.mixBlendMode !== "normal") {
+    return true;
+  }
+
+  const noneOrValueProperties = [
+    "transform",
+    "filter",
+    "perspective",
+    "clip-path",
+    "mask",
+    "mask-image",
+    "mask-border",
+  ];
+  for (let i = 0; i < noneOrValueProperties.length; i++) {
+    const property = noneOrValueProperties[i];
+    if (style[property] && style[property] !== "none") {
+      return true;
+    }
+  }
+
+  if (style.isolation && style.isolation === "isolate") {
+    return true;
+  }
+
+  // Cannot currently test the vendor prefixed values
+  /* istanbul ignore next */
+  if (
+    style["-webkit-overflow-scrolling"] &&
+    style["-webkit-overflow-scrolling"] === "touch"
+  ) {
     return true;
   }
 
--- src/index.test.js
+++ src/index.test.js
@@ -103,121 +103,311 @@ describe("findStackingContexts", () => {
         expect(contexts[0]).toHaveProperty("id", "target");
       });
     });
-  });
 
-  it("Element with a position value fixed or sticky (sticky for all mobile browsers, but not older desktop).", () => {
-    const fixed = new JSDOM(
-      `<div id="target" style="position: fixed;">
-        <div id="test-el"></div>
-      </div>`
+    it("Element with a position value fixed or sticky (sticky for all mobile browsers, but not older desktop).", () => {
+      const fixed = new JSDOM(
+        `<div id="target" style="position: fixed;">
+          <div id="test-el"></div>
+        </div>`
+      );
+      const sticky = new JSDOM(
+        `<div id="target" style="position: sticky;">
+          <div id="test-el"></div>
+        </div>`
+      );
+
+      const expectContext = [fixed, sticky];
+
+      expectContext.forEach((dom) => {
+        const testEl = dom.window.document.getElementById("test-el");
+        const contexts = findStackingContexts(testEl, dom.window);
+
+        expect(contexts).toHaveLength(2);
+        expect(contexts[0]).toHaveProperty("id", "target");
+      });
+    });
+
+    it("Element that is a child of a flex (flexbox) container, with z-index value other than auto.", () => {
+      const childOfFlexWithNoZIndex = new JSDOM(`
+        <div style="display: flex;">
+          <div>
+            <div id="test-el"></div>
+          </div>
+        </div>
+      `);
+      const childOfFlexWithAutoZIndex = new JSDOM(`
+        <div style="display: flex;">
+          <div style="z-index: auto;">
+            <div id="test-el"></div>
+          </div>
+        </div>
+      `);
+      const childOfFlexWithZIndex = new JSDOM(`
+        <div style="display: flex;">
+          <div id="target" style="z-index: 1;">
+            <div id="test-el"></div>
+          </div>
+        </div>
+      `);
+
+      const expectNoContexts = [
+        childOfFlexWithNoZIndex,
+        childOfFlexWithAutoZIndex,
+      ];
+
+      const expectContexts = [childOfFlexWithZIndex];
+
+      expectNoContexts.forEach((dom) => {
+        const testEl = dom.window.document.getElementById("test-el");
+        const contexts = findStackingContexts(testEl, dom.window);
+
+        expect(contexts).toHaveLength(1);
+      });
+
+      expectContexts.forEach((dom) => {
+        const testEl = dom.window.document.getElementById("test-el");
+        const contexts = findStackingContexts(testEl, dom.window);
+
+        expect(contexts).toHaveLength(2);
+        expect(contexts[0]).toHaveProperty("id", "target");
+      });
+    });
+
+    it("Element that is a child of a grid (grid) container, with z-index value other than auto.", () => {
+      const childOfGridWithNoZIndex = new JSDOM(`
+        <div style="display: grid;">
+          <div>
+            <div id="test-el"></div>
+          </div>
+        </div>
+      `);
+      const childOfGridWithAutoZIndex = new JSDOM(`
+        <div style="display: grid;">
+          <div style="z-index: auto;">
+            <div id="test-el"></div>
+          </div>
+        </div>
+      `);
+      const childOfGridWithZIndex = new JSDOM(`
+        <div style="display: grid;">
+          <div id="target" style="z-index: 1;">
+            <div id="test-el"></div>
+          </div>
+        </div>
+      `);
+
+      const expectNoContexts = [
+        childOfGridWithNoZIndex,
+        childOfGridWithAutoZIndex,
+      ];
+
+      const expectContexts = [childOfGridWithZIndex];
+
+      expectNoContexts.forEach((dom) => {
+        const testEl = dom.window.document.getElementById("test-el");
+        const contexts = findStackingContexts(testEl, dom.window);
+
+        expect(contexts).toHaveLength(1);
+      });
+
+      expectContexts.forEach((dom) => {
+        const testEl = dom.window.document.getElementById("test-el");
+        const contexts = findStackingContexts(testEl, dom.window);
+
+        expect(contexts).toHaveLength(2);
+        expect(contexts[0]).toHaveProperty("id", "target");
+      });
+    });
+
+    it("Element with a opacity value less than 1 (See the specification for opacity).", () => {
+      const opacityOfOne = new JSDOM(`
+        <div style="opacity: 1;">
+          <div id="test-el"></div>
+        </div>
+      `);
+      const opacityOfFiveTenths = new JSDOM(`
+        <div id="target" style="opacity: 0.5;">
+          <div id="test-el"></div>
+        </div>
+      `);
+      const opacityOfZero = new JSDOM(`
+        <div id="target" style="opacity: 0;">
+          <div id="test-el"></div>
+        </div>
+      `);
+
+      const expectNoContexts = [opacityOfOne];
+
+      const expectContexts = [opacityOfFiveTenths, opacityOfZero];
+
+      expectNoContexts.forEach((dom) => {
+        const testEl = dom.window.document.getElementById("test-el");
+        const contexts = findStackingContexts(testEl, dom.window);
+
+        expect(contexts).toHaveLength(1);
+      });
+
+      expectContexts.forEach((dom) => {
+        const testEl = dom.window.document.getElementById("test-el");
+        const contexts = findStackingContexts(testEl, dom.window);
+
+        expect(contexts).toHaveLength(2);
+        expect(contexts[0]).toHaveProperty("id", "target");
+      });
+    });
+
+    it("Element with a mix-blend-mode value other than normal.", () => {
+      const mixBlendModeNormal = new JSDOM(`
+        <div style="mix-blend-mode: normal;">
+          <div id="test-el"></div>
+        </div>
+      `);
+      const mixBlendModeMultiply = new JSDOM(`
+        <div id="target" style="mix-blend-mode: multiply;">
+          <div id="test-el"></div>
+        </div>
+      `);
+
+      const expectNoContexts = [mixBlendModeNormal];
+
+      const expectContexts = [mixBlendModeMultiply];
+
+      expectNoContexts.forEach((dom) => {
+        const testEl = dom.window.document.getElementById("test-el");
+        const contexts = findStackingContexts(testEl, dom.window);
+
+        expect(contexts).toHaveLength(1);
+      });
+
+      expectContexts.forEach((dom) => {
+        const testEl = dom.window.document.getElementById("test-el");
+        const contexts = findStackingContexts(testEl, dom.window);
+
+        expect(contexts).toHaveLength(2);
+        expect(contexts[0]).toHaveProperty("id", "target");
+      });
+    });
+
+    describe("Element with any of the following properties with value other than `none`:", () => {
+      const properties = [
+        "transform",
+        "filter",
+        // NOTE: `perspective` does not seem to be supported by the JSDOM CSSOM
+        // "perspective",
+        "clip-path",
+        "mask",
+        "mask-image",
+        "mask-border",
+      ];
+      properties.forEach((property) => {
+        // Disabling because `property` is promised to be a string
+        // eslint-disable-next-line jest/valid-title
+        it(property, () => {
+          const propertyWithNone = new JSDOM(`
+            <div style="${property}: none;">
+              <div id="test-el"></div>
+            </div>
+          `);
+          const propertyWithRandomValue = new JSDOM(`
+            <div id="target" style="${property}: 5;">
+              <div id="test-el"></div>
+            </div>
+          `);
+
+          const expectNoContexts = [propertyWithNone];
+
+          const expectContexts = [propertyWithRandomValue];
+
+          expectNoContexts.forEach((dom) => {
+            const testEl = dom.window.document.getElementById("test-el");
+            const contexts = findStackingContexts(testEl, dom.window);
+
+            expect(contexts).toHaveLength(1);
+          });
+
+          expectContexts.forEach((dom) => {
+            const testEl = dom.window.document.getElementById("test-el");
+            const contexts = findStackingContexts(testEl, dom.window);
+
+            expect(contexts).toHaveLength(2);
+            expect(contexts[0]).toHaveProperty("id", "target");
+          });
+        });
+      });
+    });
+
+    it("Element with a `isolation` value `isolate`.", () => {
+      const isolationInitial = new JSDOM(`
+        <div style="isolation: initial;">
+          <div id="test-el"></div>
+        </div>
+      `);
+      const isolationIsolate = new JSDOM(`
+        <div id="target" style="isolation: isolate;">
+          <div id="test-el"></div>
+        </div>
+      `);
+
+      const expectNoContexts = [isolationInitial];
+
+      const expectContexts = [isolationIsolate];
+
+      expectNoContexts.forEach((dom) => {
+        const testEl = dom.window.document.getElementById("test-el");
+        const contexts = findStackingContexts(testEl, dom.window);
+
+        expect(contexts).toHaveLength(1);
+      });
+
+      expectContexts.forEach((dom) => {
+        const testEl = dom.window.document.getElementById("test-el");
+        const contexts = findStackingContexts(testEl, dom.window);
+
+        expect(contexts).toHaveLength(2);
+        expect(contexts[0]).toHaveProperty("id", "target");
+      });
+    });
+
+    // Skipped because JSDOM CSSOM does support the vendor prefixed value.
+    // eslint-disable-next-line jest/no-disabled-tests
+    it.skip("Element with a `-webkit-overflow-scrolling` value `touch`.", () => {
+      const webkitOverflowScrollingInitial = new JSDOM(`
+        <div style="-webkit-overflow-scrolling: initial;">
+          <div id="test-el"></div>
+        </div>
+      `);
+      const WebkitOverflowScrollingTouch = new JSDOM(`
+        <div id="target" style="-webkit-overflow-scrolling: touch;">
+          <div id="test-el"></div>
+        </div>
+      `);
+
+      const expectNoContexts = [webkitOverflowScrollingInitial];
+
+      const expectContexts = [WebkitOverflowScrollingTouch];
+
+      expectNoContexts.forEach((dom) => {
+        const testEl = dom.window.document.getElementById("test-el");
+        const contexts = findStackingContexts(testEl, dom.window);
+
+        expect(contexts).toHaveLength(1);
+      });
+
+      expectContexts.forEach((dom) => {
+        const testEl = dom.window.document.getElementById("test-el");
+        const contexts = findStackingContexts(testEl, dom.window);
+
+        expect(contexts).toHaveLength(2);
+        expect(contexts[0]).toHaveProperty("id", "target");
+      });
+    });
+
+    it.todo(
+      "Element with a will-change value specifying any property that would create a stacking context on non-initial value"
     );
-    const sticky = new JSDOM(
-      `<div id="target" style="position: sticky;">
-        <div id="test-el"></div>
-      </div>`
+    it.todo(
+      "Element with a contain value of layout, or paint, or a composite value that includes either of them"
     );
-
-    const expectContext = [fixed, sticky];
-
-    expectContext.forEach((dom) => {
-      const testEl = dom.window.document.getElementById("test-el");
-      const contexts = findStackingContexts(testEl, dom.window);
-
-      expect(contexts).toHaveLength(2);
-      expect(contexts[0]).toHaveProperty("id", "target");
-    });
-  });
-
-  it("Element that is a child of a flex (flexbox) container, with z-index value other than auto.", () => {
-    const childOfFlexWithNoZIndex = new JSDOM(`
-      <div style="display: flex;">
-        <div>
-          <div id="test-el"></div>
-        </div>
-      </div>
-    `);
-    const childOfFlexWithAutoZIndex = new JSDOM(`
-      <div style="display: flex;">
-        <div style="z-index: auto;">
-          <div id="test-el"></div>
-        </div>
-      </div>
-    `);
-    const childOfFlexWithZIndex = new JSDOM(`
-      <div style="display: flex;">
-        <div id="target" style="z-index: 1;">
-          <div id="test-el"></div>
-        </div>
-      </div>
-    `);
-
-    const expectNoContexts = [
-      childOfFlexWithNoZIndex,
-      childOfFlexWithAutoZIndex,
-    ];
-
-    const expectContexts = [childOfFlexWithZIndex];
-
-    expectNoContexts.forEach((dom) => {
-      const testEl = dom.window.document.getElementById("test-el");
-      const contexts = findStackingContexts(testEl, dom.window);
-
-      expect(contexts).toHaveLength(1);
-    });
-
-    expectContexts.forEach((dom) => {
-      const testEl = dom.window.document.getElementById("test-el");
-      const contexts = findStackingContexts(testEl, dom.window);
-
-      expect(contexts).toHaveLength(2);
-      expect(contexts[0]).toHaveProperty("id", "target");
-    });
-  });
-
-  it("Element that is a child of a grid (grid) container, with z-index value other than auto.", () => {
-    const childOfGridWithNoZIndex = new JSDOM(`
-      <div style="display: grid;">
-        <div>
-          <div id="test-el"></div>
-        </div>
-      </div>
-    `);
-    const childOfGridWithAutoZIndex = new JSDOM(`
-      <div style="display: grid;">
-        <div style="z-index: auto;">
-          <div id="test-el"></div>
-        </div>
-      </div>
-    `);
-    const childOfGridWithZIndex = new JSDOM(`
-      <div style="display: grid;">
-        <div id="target" style="z-index: 1;">
-          <div id="test-el"></div>
-        </div>
-      </div>
-    `);
-
-    const expectNoContexts = [
-      childOfGridWithNoZIndex,
-      childOfGridWithAutoZIndex,
-    ];
-
-    const expectContexts = [childOfGridWithZIndex];
-
-    expectNoContexts.forEach((dom) => {
-      const testEl = dom.window.document.getElementById("test-el");
-      const contexts = findStackingContexts(testEl, dom.window);
-
-      expect(contexts).toHaveLength(1);
-    });
-
-    expectContexts.forEach((dom) => {
-      const testEl = dom.window.document.getElementById("test-el");
-      const contexts = findStackingContexts(testEl, dom.window);
-
-      expect(contexts).toHaveLength(2);
-      expect(contexts[0]).toHaveProperty("id", "target");
-    });
   });
 
   it("properly skips over invalid contexts", () => {
